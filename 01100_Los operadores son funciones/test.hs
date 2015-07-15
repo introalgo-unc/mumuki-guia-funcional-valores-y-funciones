67453c0ed8639d32b289833dc397a862d44db193
Mariana <mmatos87@gmail.com>
2015-07-15 14:31:16 -0300

    Se testea doble en vez de dobleDelCuadrado

--- 01100_Los operadores son funciones/test.hs
+++ 01100_Los operadores son funciones/test.hs
@@ -1,12 +1,9 @@
 describe "" $ do
-  it "dobleDelCuadrado de 4 es 32" $ do
-    dobleDelCuadrado 4 `shouldBe` 32
+  it "doble de 2 es 4" $ do
+    doble 2 `shouldBe` 4
 
-  it "dobleDelCuadrado de 2 es 8" $ do
-    dobleDelCuadrado 2 `shouldBe` 8
+  it "doble de 4 es 8" $ do
+    doble 2 `shouldBe` 4
 
-  it "dobleDelCuadrado de 1 es 2" $ do
-    dobleDelCuadrado 1 `shouldBe` 2
-
-  it "dobleDelCuadrado de 10 es 200" $ do
-    dobleDelCuadrado 10 `shouldBe` 200
+  it "doble de 13 es 26" $ do
+    doble 2 `shouldBe` 4
